1afbc02a4d9c5174fe8028539a523c4101ad886b
Benjamin Roberts <benjamin.roberts@datamininglab.com>
2021-08-02 14:00:29 -0400

    update app

--- app.py
+++ app.py
@@ -3,20 +3,14 @@ from scripts.get_scores import ItemSentiment
 
 app = Flask(__name__)
 
-"""
-@app.route('/')
-def home():
-    return render_template('home.html')
-"""
-
 @app.route('/', methods=['GET', 'POST'])
 def results():
-    data = [{
+    """data = [{
         'company': 'AAPL',
         'before_year': 2021,
         'after_year': 2000
-    }]
-    # data = request.json
+    }]"""
+    data = request.json
     scorer = ItemSentiment(data[0])
     records = scorer.run()
     return jsonify(records)
