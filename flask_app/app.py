98f4d3731d5c30fba9e505500b966f825d3d16d0
Benjamin Roberts <benjamin.roberts@datamininglab.com>
2021-09-09 13:28:43 -0400

    add flask components

---
+++ flask_app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify, render_template
+from scripts.get_scores import ItemSentiment
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def results():
+    """data = [{
+        'company': 'AAPL',
+        'before_year': 2020,
+        'after_year': 2018
+    }]"""
+    data = request.json
+    scorer = ItemSentiment(data[0])
+    records = scorer.run()
+    return jsonify(records)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
